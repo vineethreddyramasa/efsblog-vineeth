2c4bd7306c7a7595ab921fff5c6ca80f6a5e18a4
Vineeth Reddy <35588146+vineethreddyramasa@users.noreply.github.com>
2019-03-01 12:42:29 -0600

    Update settings.py

--- efsblog/settings.py
+++ efsblog/settings.py
@@ -132,9 +132,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 
 # Update database configuration with $DATABASE_URL.
